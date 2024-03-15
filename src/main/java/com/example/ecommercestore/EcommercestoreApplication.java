3581c23805bf44be3cb7331083fd43d6bdb2234f
Mahmoud <mahmoudmohm99@gmail.com>
2024-03-15 20:27:33 +0200

    db migration

--- src/main/java/com/example/ecommercestore/EcommercestoreApplication.java
+++ src/main/java/com/example/ecommercestore/EcommercestoreApplication.java
@@ -1,13 +1,29 @@
 package com.example.ecommercestore;
 
+import com.example.ecommercestore.customer.Customer;
+import com.example.ecommercestore.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class EcommercestoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercestoreApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner runner(CustomerRepository customerRepository){
+		return args -> {
+			Customer alex = new Customer(1, "Alex", "alex@gmail.com", 21);
+			Customer john = new Customer(2, "John", "john@gmail.com", 22);
+
+			List<Customer> customers = List.of(alex,john);
+			//customerRepository.saveAll(customers);
+		};
 	}
 
 }
