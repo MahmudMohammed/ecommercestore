995f22dd84f3d3d609549c7a7b99230b652b935b
Mahmoud <mahmoudmohm99@gmail.com>
2024-03-17 00:17:13 +0200

    Your descriptive commit message here

--- src/main/java/com/example/ecommercestore/customer/CustomerJDBCDataAccessService.java
+++ src/main/java/com/example/ecommercestore/customer/CustomerJDBCDataAccessService.java
@@ -10,9 +10,11 @@ import java.util.Optional;
 public class CustomerJDBCDataAccessService implements CustomerDAO{
 
     private final JdbcTemplate jdbcTemplate;
+    private final CustomerRowMapper customerRowMapper;
 
-    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate) {
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate, CustomerRowMapper customerRowMapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.customerRowMapper = customerRowMapper;
     }
 
     @Override
@@ -20,17 +22,8 @@ public class CustomerJDBCDataAccessService implements CustomerDAO{
         var sql = """
                 SELECT * FROM customer
                 """;
-        RowMapper<Customer> customerRowMapper = (rs, rowNum) -> {
-            Customer customer = new Customer(
-                    rs.getInt("id"),
-                    rs.getString("name"),
-                    rs.getString("email"),
-                    rs.getInt("age")
-            );
-            return customer;
-        };
-        List<Customer> customers = jdbcTemplate.query(sql, customerRowMapper);
-        return customers;
+
+        return jdbcTemplate.query(sql, customerRowMapper);
     }
 
     @Override
